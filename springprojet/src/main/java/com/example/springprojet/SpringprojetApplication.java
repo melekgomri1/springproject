cb5d547717ae02f9816da0d3516fe4918cb0be30
melek gomri <malekgomri881@hotmail.com>
2023-10-20 22:48:41 +0200

    environement

---
+++ springprojet/src/main/java/com/example/springprojet/SpringprojetApplication.java
@@ -0,0 +1,13 @@
+package com.example.springprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringprojetApplication.class, args);
+    }
+
+}
---
+++ springprojet/src/test/java/com/example/springprojet/SpringprojetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springprojet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
